f73e0c9c2d4b0b5acfc33e4d565d476d0d1a95ea
Williamson <6800180+xtm3x@users.noreply.github.com>
2018-03-08 00:37:04 -0500

    [SeaDoo] Resetting the uid and gid isn't necessary

--- seadoo/main.c
+++ seadoo/main.c
@@ -1,16 +1,11 @@
 #include <unistd.h>
 
 int main(int argc, char ** argv) {
-    uid_t olduid = getuid();
-    gid_t oldgid = getgid();
 
     setuid(0);
     setgid(0);
 
     int result = execvp(argv[1], &argv[1]);
-
-    setuid(olduid);
-    setgid(oldgid);
-
+    
     return result;
 }
