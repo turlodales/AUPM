75efcaaa4191ab51d4ea528d3b7b43cbb9ad575f
xTheMaster3x <xtm3xyt@gmail.com>
2018-03-09 12:39:32 -0500

    [AUPM] Fixed tab bar titles not displaying until the controller was loaded

--- AUPM/AUPMAppDelegate.m
+++ AUPM/AUPMAppDelegate.m
@@ -7,7 +7,15 @@
 	self.window.backgroundColor = [UIColor whiteColor]; //Fixes a weird visual issue after pushing a vc
 
 	UINavigationController *reposNavController = [[UINavigationController alloc] initWithRootViewController:[[AUPMRepoListViewController alloc] init]];
+	UITabBarItem *repoIcon = [[UITabBarItem alloc] initWithTitle:@"Repo" image:[UIImage imageNamed:@"Repo.png"] tag:0];
+	[repoIcon setFinishedSelectedImage:[UIImage imageNamed:@"Repo.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"Repo.png"]];
+	[reposNavController setTabBarItem:repoIcon];
+
 	UINavigationController *packagesNavController = [[UINavigationController alloc] initWithRootViewController:[[AUPMPackageListViewController alloc] init]];
+	UITabBarItem *packageIcon = [[UITabBarItem alloc] initWithTitle:@"Packages" image:[UIImage imageNamed:@"Packages.png"] tag:0];
+	[packageIcon setFinishedSelectedImage:[UIImage imageNamed:@"Packages.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"Packages.png"]];
+	[packagesNavController setTabBarItem:packageIcon];
+
 
 	self.tabBarController = [[UITabBarController alloc] init];
 	self.tabBarController.viewControllers = [NSArray arrayWithObjects:reposNavController, packagesNavController,nil];
