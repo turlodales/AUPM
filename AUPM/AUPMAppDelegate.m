c422be4375c74335ae22a9bb29ac532c2d8fc006
xTheMaster3x <xtm3xyt@gmail.com>
2018-04-09 19:43:28 -0400

    [Database] Update the database on launch instead of fully populate it

--- AUPM/AUPMAppDelegate.m
+++ AUPM/AUPMAppDelegate.m
@@ -10,27 +10,17 @@
 	self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
 	self.window.backgroundColor = [UIColor whiteColor]; //Fixes a weird visual issue after pushing a vc
 
-	//
-	// if ([[NSUserDefaults standardUserDefaults] boolForKey:@"firstSetupComplete"]) {
-	// 	UINavigationController *reposNavController = [[UINavigationController alloc] initWithRootViewController:[[AUPMRepoListViewController alloc] init]];
-	// 	UITabBarItem *repoIcon = [[UITabBarItem alloc] initWithTitle:@"Repo" image:[UIImage imageNamed:@"Repo.png"] tag:0];
-	// 	[repoIcon setFinishedSelectedImage:[UIImage imageNamed:@"Repo.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"Repo.png"]];
-	// 	[reposNavController setTabBarItem:repoIcon];
-	//
-	// 	UINavigationController *packagesNavController = [[UINavigationController alloc] initWithRootViewController:[[AUPMPackageListViewController alloc] init]];
-	// 	UITabBarItem *packageIcon = [[UITabBarItem alloc] initWithTitle:@"Packages" image:[UIImage imageNamed:@"Packages.png"] tag:0];
-	// 	[packageIcon setFinishedSelectedImage:[UIImage imageNamed:@"Packages.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"Packages.png"]];
-	// 	[packagesNavController setTabBarItem:packageIcon];
-	//
-	// 	self.tabBarController = [[UITabBarController alloc] init];
-	// 	self.tabBarController.viewControllers = [NSArray arrayWithObjects:reposNavController, packagesNavController,nil];
-	// 	self.window.rootViewController = self.tabBarController;
-	// }
-	// else {
-	AUPMDataViewController *dataLoadViewController = [[AUPMDataViewController alloc] init];
 
-	self.window.rootViewController = dataLoadViewController;
-	// }
+	if ([[NSUserDefaults standardUserDefaults] boolForKey:@"firstSetupComplete"]) {
+		AUPMDataViewController *dataLoadViewController = [[AUPMDataViewController alloc] initWithAction:1];
+
+		self.window.rootViewController = dataLoadViewController;
+	}
+	else {
+		AUPMDataViewController *dataLoadViewController = [[AUPMDataViewController alloc] init];
+
+		self.window.rootViewController = dataLoadViewController;
+	}
 
 	[self.window makeKeyAndVisible];
 }
